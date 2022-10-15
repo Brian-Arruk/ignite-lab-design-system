957b225c412de484876ca68c2245f1b1cff3a9bd
Brian-Arruk <brian.arruk@gmail.com>
2022-10-15 16:16:30 -0300

    Testing application components :fire:

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,12 +1,40 @@
 import { SignIn } from './SignIn'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
 import { Meta, StoryObj } from '@storybook/react'
-
+import { rest } from 'msw'
 
 export default {
     title: 'Pages/SignIn',
     component: SignIn,
     args: {},
-    argTypes: {}
+    argTypes: {},
+    parameters: {
+        msw: {
+            handlers: [
+                rest.post('/sessions', (req, res, ctx) => {
+                    return res(ctx.json({
+                        message: "Login realizado!"
+                    }))
+                })
+            ]
+        }
+    }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj = {
+    
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+
+        userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'brian.arruk@gmail.com')
+        userEvent.type(canvas.getByPlaceholderText('********'), '12345678')
+    
+        userEvent.click(canvas.getByRole('button'))
+    
+        await waitFor(() => {
+          return expect(canvas.getByText('Login realizado!')).toBeInTheDocument()
+        })
+    }
+}
+
