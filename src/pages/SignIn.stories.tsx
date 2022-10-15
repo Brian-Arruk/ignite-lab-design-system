3e03e5cddf278b36dc3d523c38c993e9932a3378
Brian-Arruk <brian.arruk@gmail.com>
2022-10-15 15:15:50 -0300

    fix: deploy components :bug:

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from './SignIn'
+import { Meta, StoryObj } from '@storybook/react'
+
+
+export default {
+    title: 'Pages/SignIn',
+    component: SignIn,
+    args: {},
+    argTypes: {}
+} as Meta
+
+export const Default: StoryObj = {}
